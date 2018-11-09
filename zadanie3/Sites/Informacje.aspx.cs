3bc890b361c83200aacde4d0b4a97d90522dcf3b
Michał Robaszewski <michal.robaszewski@pracuj.pl>
2018-11-09 20:44:56 +0100

    zadanie 4 problem 3 site map i menu Przenoszenie plikow

---
+++ zadanie3/Sites/Informacje.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace zadanie3
+{
+    public partial class Informacje : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- zadanie3/SzablonStrony.Master.designer.cs
+++ zadanie3/SzablonStrony.Master.designer.cs
@@ -31,6 +31,24 @@ namespace zadanie3 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// MapaSerwisu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource MapaSerwisu;
+        
+        /// <summary>
+        /// MenuGlowne control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu MenuGlowne;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
